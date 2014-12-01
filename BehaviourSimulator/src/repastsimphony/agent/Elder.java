cad8bc0f8b1954ede3de7844c5d09bcd82d326b1
Daniele Proserpio <daniele.proserpio@mail.polimi.it>
2014-12-01 17:50:06 +0100

    Elder random positioning inside the environment

---
+++ BehaviourSimulator/src/repastsimphony/agent/Elder.java
@@ -0,0 +1,33 @@
+package repastsimphony.agent;
+
+import java.util.ArrayList;
+import java.util.List;
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.query.space.grid.GridCell;
+import repast.simphony.query.space.grid.GridCellNgh;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.SpatialMath;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+import repast.simphony.util.SimUtilities;
+
+public class Elder {
+	
+	private ContinuousSpace<Object> space;
+	private Grid<Object> grid;
+	private GridPoint Target = new GridPoint(11, 11);
+	
+	
+	public Elder (ContinuousSpace<Object> space, Grid<Object> grid) {
+		this.space=space;
+		this.grid=grid;	
+	}
+
+	@ScheduledMethod(start = 1, interval = 1)
+	public void step() {
+	
+		GridPoint pt = grid.getLocation(this);		
+	}	
+}
--- BehaviourSimulator/src/repastsimphony/common/Constants.java
+++ BehaviourSimulator/src/repastsimphony/common/Constants.java
@@ -1,5 +1,7 @@
 package repastsimphony.common;
 
 public class Constants {
+	
+	public static final int mapSize = 30;
 
 }
